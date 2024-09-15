d5fb31073706abad100d7d8c344465f1e926f1f7
n-harshada03 <harshdanavale2003@gmail.com>
2024-09-15 16:03:20 +0530

    added dependencies for project

---
+++ quickstart/src/test/java/com/accelerate/quickstart/QuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accelerate.quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
