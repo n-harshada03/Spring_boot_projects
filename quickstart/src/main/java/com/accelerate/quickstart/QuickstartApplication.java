fd724c42b5c27709222801d3762d9fb1683f7d99
n-harshada03 <harshdanavale2003@gmail.com>
2024-09-15 17:08:08 +0530

    deployed simple “Hello World” web service in Spring Boot.

--- quickstart/src/main/java/com/accelerate/quickstart/QuickstartApplication.java
+++ quickstart/src/main/java/com/accelerate/quickstart/QuickstartApplication.java
@@ -2,12 +2,31 @@ package com.accelerate.quickstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+
+//
 @SpringBootApplication
+@RestController   //tells Spring that this code describes endpoint that should be made available over the web.
 public class QuickstartApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(QuickstartApplication.class, args);
 	}
 
+	@GetMapping("/hello")  // tells Spring that use hello() method to answer requests
+	public String hello(@RequestParam(value = "name",defaultValue = "World") String name){
+		return String.format("Hello %s!",name );
+
+    }
+
 }
+
+
+
+//  Spring Boot’s embedded Apache Tomcat server is acting as a webserver and is listening for
+//  requests on localhost port 8080. Open your browser and in the address bar at the top
+//  enter http://localhost:8080/hello
