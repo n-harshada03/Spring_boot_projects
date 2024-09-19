6b3470834fd09dcc9a658d33214cca9670b7dd87
n-harshada03 <harshdanavale2003@gmail.com>
2024-09-19 22:55:09 +0530

    other files

---
+++ resthateoas/resthateoas/src/test/java/com/accelerate/resthateoas/ResthateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accelerate.resthateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResthateoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
