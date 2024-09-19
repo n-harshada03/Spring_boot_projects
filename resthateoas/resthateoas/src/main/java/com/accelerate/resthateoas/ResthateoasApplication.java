6303eb2b16a76d9e19c52a52c4ebf8fbce70d9a3
n-harshada03 <harshdanavale2003@gmail.com>
2024-09-19 22:53:43 +0530

    main class

---
+++ resthateoas/resthateoas/src/main/java/com/accelerate/resthateoas/ResthateoasApplication.java
@@ -0,0 +1,13 @@
+package com.accelerate.resthateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResthateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResthateoasApplication.class, args);
+	}
+
+}
